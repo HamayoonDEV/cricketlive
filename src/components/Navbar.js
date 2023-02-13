30103db674c64132881590ba69245d9e6821bef8
unknown <hamayoon.47611@iqra.edu.pk>
2023-02-14 02:26:49 +0500

    added navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Toopak</h1>
+      <h3>Sports</h3>
+    </div>
+  );
+};
+
+export default Navbar;
